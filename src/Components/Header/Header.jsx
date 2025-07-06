36df7f5f756f64f8e2ccb29386cd566158913cb6
Isuru <isuruindrajith680@gmail.com>
2025-07-06 11:23:23 +0530

    navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './Header.css'
+import MenuLink from '../MenuLink/MenuLink'
 
 function Header() {
   return (
     <div id="navbar">
-      <h1>My Portfolio</h1>
+          <h1>My Portfolio</h1>
+          
+          <div>
+              <MenuLink linkname="Home" url="#home"/>
+              <MenuLink linkname="About" url="#about"/>
+              <MenuLink linkname="Projects" url="#projects" />
+              <MenuLink linkname="Skills & Achievements" url="#skills" />
+              <MenuLink linkname="Education" url="#education" />
+              <MenuLink linkname="Experience" url="#experience" />
+              <MenuLink linkname="Contacts" url="#contacts" />
+          </div>
     </div>
   )
 }
