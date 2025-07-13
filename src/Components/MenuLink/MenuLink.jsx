11fe5a658d198161203456161765e74755a4f5c0
Isuru <isuruindrajith680@gmail.com>
2025-07-13 08:43:40 +0530

    Link from react-router-dom

--- src/Components/MenuLink/MenuLink.jsx
+++ src/Components/MenuLink/MenuLink.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './MenuLink.css'
+import { Link } from 'react-router-dom';
 
 function MenuLink(props) {
   return (
     <div>
-          <a href={props.url} className='link'>{props.linkname}</a>
+          <Link to={props.url} className='link'>{props.linkname}</Link>
+          
     </div>
   )
 }
